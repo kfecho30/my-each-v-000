ffff940b342d9481a89a94448b364fd718b33ec9
Kelsey Fecho <kfecho30@gmail.com>
2018-03-07 20:02:01 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  counter = 0
+  while counter < array.length
+    yeild(array[counter])
+    counter += 1
+  end
+  array
 end
